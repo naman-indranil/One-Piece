cd797ba238cb2fb048fa9d59ebc67e5d5403c0bd
namanindranil2299 <itscallednamanindranil@gmail.com>
2020-09-13 15:02:47 +0530

    commit day 0

---
+++ subsetmex.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        ll a[101] = {0};
+        ll temp, ansa = 0, ansb = 0;
+        // memset(a, 0, sizeof(a));
+        ll A[101] = {0}, B[101] = {0};
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> temp;
+            if (a[temp] == 0)
+            {
+                a[temp] += 1;
+                A[temp] = 1;
+            }
+            else if (a[temp] > 0)
+            {
+                a[temp] += 1;
+                B[temp] = 1;
+            }
+        }
+        temp = 0;
+        while (A[temp])
+        {
+            temp++;
+            ansa = temp;
+        }
+        temp = 0;
+        while (B[temp])
+        {
+            temp++;
+            ansb = temp;
+        }
+        temp = ansa + ansb;
+        cout << temp << endl;
+    }
+    return 0;
+}
